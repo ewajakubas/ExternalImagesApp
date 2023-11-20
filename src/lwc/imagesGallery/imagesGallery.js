8aae871d4f110a39dbd1e931e2574398974996dd
ejakubas <ejakubas13@gmail.com>
2023-11-20 10:33:28 -0500

    new changes

--- src/lwc/imagesGallery/imagesGallery.js
+++ src/lwc/imagesGallery/imagesGallery.js
@@ -1,16 +1,14 @@
-// picsumImages.js
-import { LightningElement, wire, track } from 'lwc';
-import getImages from '@salesforce/apex/PicsumImagesController.getImages';
-
+import { LightningElement, wire } from 'lwc';
+import getImages from '@salesforce/apex/PhotoController.getImages';
 
 export default class imagesGallery extends LightningElement {
+
     isLoading = true;
     searchTerm = '';
-    @track images = [];
-    @track filteredImages = [];
-    @track noMatchingRecords = false;
-    @track noRecords = false;
-
+    images;
+    filteredImages;
+    noMatchingRecords = false;
+    noRecords = false;
 
     @wire(getImages)
     wiredImages({ error, data }) {
@@ -30,20 +28,18 @@ export default class imagesGallery extends LightningElement {
                              this.isLoading = false;
                          }, 2000);
         } else if (error) {
-            // Handle the error
             console.error('Error fetching images:', error);
         }
     }
 
     handleSearch(event) {
-        console.log('???????????????????? ! 1 this.filteredImages' + this.filteredImages);
         this.searchTerm = event.target.value.toLowerCase();;
         this.filterImages();
     }
 
     filterImages() {
         this.filteredImages = this.images.filter(image => image.title.toLowerCase().includes(this.searchTerm));
-        if ( this.filteredImages.length === 0) {
+        if ( this.filteredImages.length == 0) {
             this.noMatchingRecords = true;
         }
     }
